6932619f7b2a562bd3ebb2de6e1666f678cc98f6
Aitor <aitorpalomares@hotmail.com>
2021-05-24 22:57:53 +0100

    add btn validator

--- src/App/Filters/Filters.tsx
+++ src/App/Filters/Filters.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import Form from '../../components/Form';
+import moment from 'moment';
+import { Form as RFFForm } from 'react-final-form';
 import Button from '../../components/Button';
 import SelectCountry from '../../components/SelectCountry';
 import DateRange from '../../components/DateRange';
 import { Props, State } from './Filters.interfaces';
 
+const isValid = (values: any = {}) => {
+  const { dates = {}} = values;
+
+  return (
+    (values["from"] !== undefined) &&
+    (values["to"] !== undefined) &&
+    (dates["from"] !== undefined) &&
+    (dates["to"] !== undefined)
+  );
+}
 const Filtes = ({
   isLoading = false,
   onSubmit
 }: Props & State) => {
-  const handleSubmit = (values: any) => {
-    onSubmit(values);
-  }
+  const handleOnSubmit = (values: any) => {
+    const { from = [], to = [], dates = {} } = values;
+    
+    onSubmit({
+      from: from.map((f: any) => f.value).join(','),
+      to: to.map((t: any) => t.value).join(','),
+      dateFrom: moment(dates.from).format('DD/MM/YYYY'),
+      dateTo: moment(dates.to).format('DD/MM/YYYY'),
+    });
+  };
+
   return (
-    <div>
-      <Form onSubmit={handleSubmit} onChange={(e: any) => console.log(e)}>
-        <SelectCountry name='from' label="From" />
-        <SelectCountry name='to' label="To" />
-        <DateRange name="dates" />
-        <Button label="Search" loading={isLoading} />
-      </Form>
-    </div>
+    <RFFForm onSubmit={handleOnSubmit} onChange={(e: any) => console.log(e)}>
+      {({ values, handleSubmit }: any) => (
+        <form onSubmit={handleSubmit}>
+          <SelectCountry name='from' label="From" />
+          <SelectCountry name='to' label="To" />
+          <DateRange name="dates" />
+          <Button label="Search" loading={isLoading} disabled={!isValid(values)} />
+        </form>
+      )}
+    </RFFForm>
   );
 };
 export default Filtes;
